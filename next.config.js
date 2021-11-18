535e29cc75ea228d0d0d0b7029ebc598ac153d1d
Stackbit Code Editor <projects@stackbit.com>
2021-11-18 11:42:11 +0200

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const sourcebit = require('sourcebit');
-const sourcebitConfig = require('./sourcebit.js');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
