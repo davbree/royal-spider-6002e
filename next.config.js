727986beef702a95bb903fcf92b078d29b862fd0
Stackbit Code Editor <projects@stackbit.com>
2021-11-18 11:45:12 +0200

    next.config.js: changed

--- next.config.js
+++ next.config.js
@@ -16,12 +16,14 @@ module.exports = withBundleAnalyzer({
     },
     webpack: (config, { webpack, dev, isServer }) => {
 
+        if (isServer) {
 try {
     sourcebit.fetch(sourcebitConfig).catch(err => console.log(err))
 } catch (err) {
 
 
 }
+        }
         // temporary: for local development -  edit package.json "@stackbit/components": "file:../stackbit-components" and uncomment the alias below.
         // config.resolve.alias['react'] = path.resolve('./node_modules/react');
         // config.resolve.alias['next'] = path.resolve('./node_modules/next');
