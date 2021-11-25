fb209fc9cb01f9cad9a465ea62163f6dfb958048
Stackbit Code Editor <projects@stackbit.com>
2021-11-25 11:02:56 +0200

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import '../components/register-components';
 import '../css/main.css';
 import Head from 'next/head';
-
+//dsds
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
